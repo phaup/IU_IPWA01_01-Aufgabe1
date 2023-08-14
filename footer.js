a16eccc210581fb75d31a7f8edf3d66fd2473d28
phaup <141155958+phaup@users.noreply.github.com>
2023-08-14 09:55:24 +0200

    Add files via upload

--- footer.js
+++ footer.js
@@ -1,13 +1,22 @@
 // footer.js
+
+// Funktion, um den Footer der Webseite zu rendern
 function renderFooter() {
+  // Rückgabe des HTML-Codes für den Footer
   return `
+    <!-- Footer-Bereich mit festem Boden (fixed-bottom) und hellen Hintergrund -->
     <footer class="fixed-bottom bg-light">
+      <!-- Container für die Anordnung der Inhalte im Footer, vertikal zentriert -->
       <div class="container d-flex align-items-center">
+        <!-- Bereich für den Copyright-Hinweis -->
         <div class="mb-0">
           <p>&copy; 2023 CO2-Footprint Webseite der EarthGuardians. Alle Rechte vorbehalten.</p>
         </div>
+        <!-- Bereich für die Navigation (Links) im Footer -->
         <nav>
+          <!-- Unsortierte Liste für die Links im Footer, in einer Zeile (list-inline) -->
           <ul class="list-inline">
+            <!-- Einzelner Listeneintrag (Listenelement) mit Inline-Stil -->
             <li class="list-inline-item"><a href="datenschutzerklaerung.html">Datenschutzerklärung</a></li>
             <li class="list-inline-item"><a href="impressum.html">Impressum</a></li>
           </ul>
